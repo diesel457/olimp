fd1fede2dc3fe8a3c498a30d4be62b6dc9109f6c
Dima <dimarakov21@gmail.com>
2016-12-17 12:22:04 +0300

    fixed header

--- apps/main/pages/Rooms/components/AddRoom/AddRoom.jsx
+++ apps/main/pages/Rooms/components/AddRoom/AddRoom.jsx
@@ -6,7 +6,31 @@ class AddRoom extends Component {
   render () {
     return (
       <div className='AddRoom'>
-        Add room
+        <div className='AddRoom-content'>
+          <div className='AddRoom-form'>
+
+            <div className='AddRoom-form-row'>
+              <label htmlFor='input1'>Загрузить фотографию карточки комнаты</label>
+              <input id='input1' type='file'/>
+            </div>
+
+  					<div className='AddRoom-form-row'>
+              <label htmlFor='input2'>Заголовок карточки комнаты</label>
+              <input id='input2' type='text'/>
+            </div>
+
+            <div className='AddRoom-form-row -textarea'>
+              <label htmlFor='input3'>Описание карточки комнаты</label>
+              <textarea id='input3'></textarea>
+            </div>
+
+            <div className='AddRoom-form-row'>
+              <label htmlFor='input4'>Прайс карточки комнаты</label>
+              <input id='input4' type='number'/>
+            </div>
+
+  				</div>
+        </div>
       </div>
     )
   }
